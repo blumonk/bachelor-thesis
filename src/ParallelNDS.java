4dd3158abe0acbe3404c5e2b5060240f181aaa21
Anatoly Kim <akim@cloudlinux.com>
2017-05-16 13:10:38 +0300

    specification

--- src/ParallelNDS.java
+++ src/ParallelNDS.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2015 Maxim Buzdalov
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -45,11 +29,6 @@ public final class ParallelNDS {
         }
     }
 
-    /**
-     * A base class for all sorters.
-     * A sorter supports two getter methods (for size and dimension)
-     * and the method for actual sorting.
-     */
     public static abstract class Sorter {
         protected final int size;
         protected final int dim;
@@ -59,42 +38,14 @@ public final class ParallelNDS {
             this.dim = dim;
         }
 
-        /**
-         * Returns the size of the problem this sorter can handle.
-         *
-         * @return the size of the problem.
-         */
         public int size() {
             return size;
         }
 
-        /**
-         * Returns the dimension of the problem this sorter can handle.
-         *
-         * @return the dimension of the problem.
-         */
         public int dimension() {
             return dim;
         }
 
-        /**
-         * Performs the non-dominated sorting of the given input array
-         * and stores the results in the given output array.
-         * <p>
-         * The input array should have the dimensions of exactly {#size()} * {#dimension()},
-         * otherwise an IllegalArgumentException is thrown.
-         * <p>
-         * The output array should have the dimension of exactly {#size()},
-         * otherwise an IllegalArgumentException is thrown.
-         * <p>
-         * The method does not change the {#input} array and fills the {#output} array by layer indices:
-         * <code>i</code>th element of {#output} will be the layer index of the <code>i</code>th point from {#input}.
-         * The layer 0 corresponds to the non-dominated layer of solutions, the layer 1 corresponds to solutions which
-         * are dominated by solutions from layer 0 only, and so far.
-         *
-         * @param input  the input array which is to be sorted.
-         * @param output the output array which is filled with the front indices of the corresponding input elements.
-         */
         public void sort(double[][] input, int[] output) {
             if (input.length != size) {
                 throw new IllegalArgumentException(
